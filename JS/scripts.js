c0cf439e379d72e63cffa7c2f3ec684afc7ca4e6
hectorestebanm <109253700+hectorestebanm@users.noreply.github.com>
2022-09-15 14:51:01 -0300

    Add files via upload

---
+++ JS/scripts.js
@@ -0,0 +1,11 @@
+function submit()
+{
+    var span = document.querySelector(".card-end__p1__span");
+    var buttons = document.querySelectorAll(".card__button");
+    var submit = document.querySelector(".card-submit");
+    var cardRating = document.querySelector(".card-rating");
+    var cardEnd = document.querySelector(".card-end");
+    
+    cardRating.style.display = "none";
+    cardEnd.style.display = "flex";
+}

b0bff74abd0bbc999aa86203a557186d8729c420
hectorestebanm <109253700+hectorestebanm@users.noreply.github.com>
2022-09-22 16:45:51 -0300

    Add files via upload

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,11 +1,19 @@
-function submit()
+var span = document.querySelector(".card-end__p1__span");
+var buttons = document.querySelectorAll(".card__button");
+var submit = document.querySelector(".card__submit");
+var cardRating = document.querySelector(".card-rating");
+var cardEnd = document.querySelector(".card-end");
+
+buttons.forEach(function(number)
 {
-    var span = document.querySelector(".card-end__p1__span");
-    var buttons = document.querySelectorAll(".card__button");
-    var submit = document.querySelector(".card-submit");
-    var cardRating = document.querySelector(".card-rating");
-    var cardEnd = document.querySelector(".card-end");
-    
+    number.addEventListener("click", function()
+    {
+        span.innerHTML = number.innerHTML;
+    });
+});
+
+submit.addEventListener("click", function()
+{
     cardRating.style.display = "none";
     cardEnd.style.display = "flex";
-}
+});
